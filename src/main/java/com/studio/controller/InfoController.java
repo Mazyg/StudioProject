7660668f38ef1858e1929002579142cf7ba65024
zcll <15270127274@163.com>
2019-12-06 16:23:37 +0800

    电影，书籍页面

--- src/main/java/com/studio/controller/InfoController.java
+++ src/main/java/com/studio/controller/InfoController.java
@@ -2,18 +2,15 @@ package com.studio.controller;
 
 
 import com.studio.domian.Info;
-import com.studio.domian.Topic;
 import com.studio.service.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -80,6 +77,30 @@ public class InfoController {
         return "user/main/personal";
     }
 
+    /**
+     * 显示电影信息
+     * @param model
+     * @return
+     */
+    @RequestMapping("/findMovies")
+    public String findMovie(Model model){
+        List<Info> movies = infoService.findMovies();
+        model.addAttribute("movies",movies);
+        return "user/main/movies";
+    }
+
+    /**
+     * 显示书籍信息
+     * @param model
+     * @return
+     */
+    @RequestMapping("/findBooks")
+    public String findBook(Model model){
+        List<Info> books = infoService.findBooks();
+        model.addAttribute("books",books);
+        return "user/main/books";
+    }
+
     @RequestMapping("/findById")
     public ModelAndView findById(String info_id){
         mv = new ModelAndView();
@@ -95,8 +116,10 @@ public class InfoController {
     }
 
     @RequestMapping("/findByTitle")
-    public String findByTitle(){
-        return "";
+    public String findByTitle(Model model,String title){
+        Info info = infoService.findByTitle(title);
+        model.addAttribute("movie",info);
+        return "user/main/movie";
     }
 
     @RequestMapping("/saveInfo")
