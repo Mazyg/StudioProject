15dea79af20a030f2045095466220dea2e3780c2
zcl <15270127274@163.com>
2019-12-08 21:43:19 +0800

    话题-回复页面修整+回复功能

--- src/main/java/com/studio/dao/CommentDao.java
+++ src/main/java/com/studio/dao/CommentDao.java
@@ -1,6 +1,7 @@
 package com.studio.dao;
 
 import com.studio.domian.Comment;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,8 @@ import java.util.List;
 @Repository
 public interface CommentDao {
 
+    @Insert("insert into comment(date,uname,rname,wid,content) " +
+            "values('${date}','${uname}','${rname}','${wid}','${content}')")
     public boolean saveComment(Comment comment);
 
     public boolean deleComment(Comment comment);
