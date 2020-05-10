f21f84db28b7f786344996ff14cc7709d0c3f474
zcl <15270127274@163.com>
2020-05-10 12:01:41 +0800

    完善反馈模块

--- src/main/java/com/studio/service/FeedbackService.java
+++ src/main/java/com/studio/service/FeedbackService.java
@@ -5,7 +5,9 @@ import com.studio.domian.Feedback;
 import java.util.List;
 
 public interface FeedbackService {
-    public boolean addFeedback(Feedback feedback);
+    boolean addFeedback(Feedback feedback);
 
     List<Feedback> getAllFeedback();
+
+    Feedback findById(Integer fid);
 }
